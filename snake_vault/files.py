26803cc68ca57d313fb498ffb0935be6be08723c
Pascal Malouin <pascal.malouin@gmail.com>
2024-03-20 07:16:32 -0400

    Updated using lazygit...

--- snake_vault/files.py
+++ snake_vault/files.py
@@ -2,62 +2,60 @@
 ## [snake_vault/files.py]
 ## author        : fantomH
 ## created       : 2024-02-23 20:11:19 UTC
-## updated       : 2024-02-23 20:11:19 UTC
+## updated       : 2024-03-20 10:32:55 UTC
 ## description   : Files utilities.
 
-import os
-import hashlib
 import csv
+import hashlib
+import os
+import magic
 
-def file_info(f):
+def file_info(f, filename=True,
+                 path=True,
+                 size=True,
+                 mime=True,
+                 md5=True):
     '''
     f must be an absolute path.
     
-    For exemple:
-    PATH = '/home/ghost/.ael'
-    for f in os.listdir(PATH):
-        FILE_INFO = file_info(os.path.join(PATH, f))
-        if FILE_INFO:
-            print(FILE_INFO)
     '''
 
-    if os.path.isfile(f):
+    FILE_INFO = {}
 
-        try:
-            FILE_INFO = {}
-            FILE_INFO['path'] = os.path.abspath(f)
+    if os.path.isfile(f):
+        if filename:
             FILE_INFO['filename'] = os.path.basename(f)
+
+        if path:
+            FILE_INFO['path'] = os.path.abspath(f)
+
+        if size:
             FILE_INFO['size'] = os.path.getsize(f)
 
+        if md5:
             md5 = hashlib.md5()
-            with open(FILE_INFO.get('path'), 'rb') as _INPUT:
+            filepath = os.path.abspath(f).encode('UTF-8', 'surrogateescape')
+            with open(filepath, 'rb') as _INPUT:
                 for line in _INPUT:
                     md5.update(line)
                 FILE_INFO['md5sum'] = md5.hexdigest()
+        if mime:
+            FILE_INFO['mime'] = magic.from_file(f, mime=True)
 
-            return FILE_INFO
-        except:
-            FILE_INFO = {}
-            FILE_INFO['path'] = "PASS"
-            FILE_INFO['filename'] = "PASS"
-            FILE_INFO['size'] = "PASS"
-            FILE_INFO['md5sum'] = "PASS"
+    else:
+        return
 
-            return FILE_INFO
-    return False
+    return FILE_INFO
 
-list_of_directories = [ '/home/ghost/main/radimed/' ]
-_newline = "\n"
-with open('/home/ghost/tmp/filesmd5.csv', mode="w") as _output:
-    fieldnames = ['filename', 'path', 'size', 'md5sum']
-    writer = csv.DictWriter(_output, fieldnames=fieldnames)
+def _test_file_info():
 
-    for directory in list_of_directories:
-        for root, dirs, files in os.walk(directory):
-            if "/.git/" not in root:
-                for file in files:
-                    _info = file_info(os.path.join(root.encode('UTF-8', 'surrogateescape'), file.encode('UTF-8', 'surrogateescape')))
-                    writer.writerow(_info)
+    PATH = '/usr/local/bin'
+    for root, dirs, files in os.walk(PATH):
+        for file in files:
+            print(file_info(os.path.join(root, file), mime=True))
+
+if __name__ == "__main__":
+    _test_file_info()
 
 # vim: foldmethod=marker
 ## ------------------------------------------------------------- FIN ¯\_(ツ)_/¯
